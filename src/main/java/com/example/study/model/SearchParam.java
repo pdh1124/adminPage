1c7ab96f31d61354083aec6f0ead115ea9086fac
pdh1124 <pdw3231@naver.com>
2021-02-27 21:23:22 +0900

    Lombok  - 플러그인 설치 및 설정

--- src/main/java/com/example/study/model/SearchParam.java
+++ src/main/java/com/example/study/model/SearchParam.java
@@ -1,12 +1,22 @@
 package com.example.study.model;
 
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data //lombok을 연결 (플러그인 설치 후 build gradle에 lombok의 대한 소스를 넣으면 사용 가능)
+@AllArgsConstructor //모든 아규먼트를 가진 생성자를 추가 한다고 한다면
 public class SearchParam {
+
     private String account;
     private String email;
     private int page;
 
     //{ "account" : "", "email" : "", "page" : 0}
 
+
+    /*
     //해당 파라미터를 받기 위해서는 get,set메소드와 생성자가 있어야 함
     public String getAccount() {
         return account;
@@ -31,4 +41,26 @@ public class SearchParam {
     public void setPage(int page) {
         this.page = page;
     }
+
+    
+    //생성자 메소드 추가
+    public SearchParam() {
+
+    }
+
+    //생성자 메소드에 필요한 매개변수를 추가
+    public SearchParam(String account, String email, int page) {
+        this.account = account;
+        this.email = email;
+        this.page = page;
+    }
+
+    //필요한 부분의 매개변수만을 넣어서 사용
+    public SearchParam(String account) {
+        this.account = account;
+    }
+    */
+
+
+
 }
